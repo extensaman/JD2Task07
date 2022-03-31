daeea3470d560d85c89ed86f9694eaf042b49d1d
extensaman <verus.wedding@gmail.com>
2022-03-31 23:59:19 +0300

    H2 connected (for tests)

--- src/test/java/by/academy/it/task07/AppTest.java
+++ src/test/java/by/academy/it/task07/AppTest.java
@@ -4,11 +4,23 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
 public class AppTest
 {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() throws SQLException {
+        ResourceBundle bundle = ResourceBundle.getBundle("database");
+        String url = bundle.getString("urlForTest");
+        String user = bundle.getString("userForTest");
+        String password = bundle.getString("passwordForTest");
+        Connection connection = null;
+
+        connection = DriverManager.getConnection(url, user, password);
+
+        assertTrue( connection != null );
     }
 }
